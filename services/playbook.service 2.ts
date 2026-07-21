c734c8799cf81955d69e2f24e09c4e734b8700e2
PrishaDureja <durejaprisha17@gmail.com>
2026-07-21 11:57:17 +0530

    chore: cleanup iCloud sync conflict files

--- services/playbook.service 2.ts
+++
@@ -1,6 +0,0 @@
-import { api } from '@/services/api';
-import type { PlaybookWeek } from '@/types/playbook';
-
-export const playbookService = {
-  getAll: () => api.get<PlaybookWeek[]>('/playbook'),
-};
